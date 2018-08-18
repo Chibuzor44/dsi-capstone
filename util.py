25067eae9b3856dcb5401d1da008a540e75b80fe
EC2 Default User <ec2-user@ip-172-16-14-221.us-east-2.compute.internal>
2018-08-18 02:04:58 +0000

    removed if name==main, state print and length

--- util.py
+++ util.py
@@ -170,15 +170,3 @@ def display(model, df, first_star, second_star, state=None, bus_name=None):
     return df_pos, df_neg, name, size, Recall, Precision, Accuracy, lst_neg[:15], lst_pos[:15]
 
     
-if __name__ == "__main__":
-    from nltk.tokenize import RegexpTokenizer
-    from nltk.stem.wordnet import WordNetLemmatizer
-    from nltk.stem.porter import PorterStemmer
-    from nltk.corpus import stopwords
-    sw = set(stopwords.words("english"))
-    tokenizer = RegexpTokenizer("[\w']+")
-    st = PorterStemmer()
-
-    cup = ["Deserves a zero. I was highly disappointed. We went for my husband's birthday expecting a meal worthy of the $200 we spent.  The food was very bland. I was so upset I didn't even bother to order dessert.  I thought if dessert tastes anything like dinner, I'm not going to bother. Noone bothered to ask how the food was. The chef didn't bother to come to our table,  even though he spoke to individuals at the three other tables surrounding us. Thank God I opted for the indoor gondola ride after dinner which helped to improve my mood for the evening.  I won't be back here or to any of his restaurants. I'm a good person so I still left a very generous tip;  after all it wasn't the waiter's fault the food was just ok.",
-    "This was by far the worst meal I have ever had.  Not steak,   Meal!  I went with a group of 4, only 1 steak was cooked correctly and it was super fatty. I had a filet ordered medium to medium well black, it showed up well done with no char whatsoever.  My friend ordered hers blue and it showed up rare. My husband's was fine but like I said super fatty. The other friends was over cooked as well along with the snottiest looking disgusting foie gras I have ever seen. Bottom line_ you go to Vegas to have a great meal and it sucked.  All of it sucked. Go somewhere else for $100pp."]
-    print(clean_stem(cup, tokenizer, st, sw))
